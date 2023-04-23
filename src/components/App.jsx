88e4c73c73eabbfbeb148e2bee6d361abf541f2b
VovaKyrychuk <kyrychuk81@gmail.com>
2023-04-24 00:26:43 +0300

    step 1-3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout/Layout';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  leaveFeedback = element => {
+    this.setState({ [element]: this.state[element] + 1 });
+  };
+
+  countTotalFeedback = ({ good, neutral, bad }) => good + neutral + bad;
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      (this.state.good / this.countTotalFeedback(this.state)) * 100
+    );
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Layout>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            onLeaveFeedback={this.leaveFeedback}
+            options={Object.keys(this.state)}
+          />
+        </Section>
+        {this.countPositiveFeedbackPercentage() > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback(this.state)}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+p {
+  margin: 16px 0;
+  font-weight: bold;
+  color: #0000ff;
+}
+ul, ol {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+button {margin-right: 5px}`;
